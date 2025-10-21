4b5ce58387197af8e24c801f1ddabbd1a4e2f813
hoyong.shin <hoyong6737@gmail.com>
2025-10-21 19:29:42 +0900

    Gemini_API 오류로 모델 이름 수정, main.py_관련도 점수 오류로 수정

--- web-crawler/analysis/gemini_analyzer.py
+++ web-crawler/analysis/gemini_analyzer.py
@@ -20,7 +20,7 @@ genai.configure(api_key=GEMINI_API_KEY)
 #         print(m.name)
 
 # 사용할 Gemini 모델 설정
-model = genai.GenerativeModel('models/gemini-flash-latest')
+model = genai.GenerativeModel('gemini-flash-latest')
 
 
 
